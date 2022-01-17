1c28bc2b5d19eca25d438648278d9b958a86f70f
ahmedmsuliman19 <ahmed.suliman75753@gmail.com>
2022-01-17 14:12:04 +0400

    Created our entities and refactored using project Lombok

--- src/main/java/com/example/springIt/SpringItApplication.java
+++ src/main/java/com/example/springIt/SpringItApplication.java
@@ -1,13 +1,18 @@
 package com.example.springIt;
 
+import com.example.springIt.config.SpringitProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class SpringItApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringItApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringItApplication.class, args);
-		System.out.println("Welcome to Springit");
 	}
 }
---
+++ src/main/java/com/example/springIt/domain/Vote.java
@@ -0,0 +1,22 @@
+package com.example.springIt.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private int vote;
+
+}
