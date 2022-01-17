7394510cd25c3ce1a36e140285ec5b8df4626ba0
ahmedmsuliman19 <ahmed.suliman75753@gmail.com>
2022-01-17 14:41:58 +0400

    Initial repositories for Link and Comment

--- src/main/java/com/example/springIt/SpringItApplication.java
+++ src/main/java/com/example/springIt/SpringItApplication.java
@@ -1,11 +1,9 @@
 package com.example.springIt;
 
-import com.example.springIt.config.SpringitProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class SpringItApplication {
