fbece211fc1185d31acb57c344b4a42f086768d8
ahmedmsuliman19 <ahmed.suliman75753@gmail.com>
2022-01-17 10:57:35 +0400

    Initial Commit for Our Reddit Clone

---
+++ src/test/java/com/example/springIt/SpringItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
