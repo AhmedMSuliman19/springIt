7d1a87051804edcf1fd01e7b9830ca339c9b2a4e
ahmedmsuliman19 <ahmed.suliman75753@gmail.com>
2022-01-19 10:28:17 +0400

    Add links view and link submission functionality

--- src/main/java/com/example/springIt/SpringItApplication.java
+++ src/main/java/com/example/springIt/SpringItApplication.java
@@ -1,12 +1,8 @@
 package com.example.springIt;
 
-import com.example.springIt.domain.Comment;
-import com.example.springIt.domain.Link;
-import com.example.springIt.repository.CommentRepository;
-import com.example.springIt.repository.LinkRepository;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,15 +19,8 @@ public class SpringItApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
-		return args -> {
-			Link link = new Link("Getting started with spring boot 2", "https://therealdanvega.com");
-			linkRepository.save(link);
-
-			Comment comment = new Comment("This spring boot 2 link is awesome!", link);
-			commentRepository.save(comment);
-			link.addComment(comment);
-
-		};
+	PrettyTime prettyTime() {
+		return new PrettyTime();
 	}
+
 }
