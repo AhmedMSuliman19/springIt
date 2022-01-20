8990d0fc54c0e574c5f4c91ac0668b33438e2835
ahmedmsuliman19 <ahmed.suliman75753@gmail.com>
2022-01-20 10:00:18 +0400

    Cleaning up security for voting mechanism

--- src/main/java/com/example/springIt/domain/Vote.java
+++ src/main/java/com/example/springIt/domain/Vote.java
@@ -1,22 +1,27 @@
 package com.example.springIt.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @NoArgsConstructor
-@Data
-public class Vote {
+@RequiredArgsConstructor
+@Getter @Setter
+public class Vote extends Auditable{
 
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
-    private int vote;
+    private short direction;
+
+    @NonNull
+    @ManyToOne
+    private Link link;
 
 }
---
+++ src/main/java/com/example/springIt/repository/VoteRepository.java
@@ -0,0 +1,8 @@
+package com.example.springIt.repository;
+
+import com.example.springIt.domain.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+}
